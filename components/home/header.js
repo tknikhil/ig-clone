fdba321a0314157241a3ce213c2beaf083f9db93
Nikhil TK <nikhiltk1114@gmail.com>
2023-04-05 17:26:09 +0530

    need to add icon

--- components/home/header.js
+++ components/home/header.js
@@ -1,19 +1,48 @@
-import { View, Image, StyleSheet } from "react-native";
+import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
 import React from "react";
 
 const Header = () => {
   return (
-    <View>
-      <Image style={styles.logo} source={require("../../assets/ig_name.png")} />
+    <View style={styles.container}>
+      <TouchableOpacity>
+        <Image
+          style={styles.logo}
+          source={require("../../assets/ig_name.png")}
+        />
+      </TouchableOpacity>
+      <View style={styles.iconsContainer}>
+        <TouchableOpacity>
+          <Image
+            style={styles.icons}
+            source={{ url: "https://icons8.com/icon/37787/add-new" }}
+          />
+        </TouchableOpacity>
+        <Text style={{ color: "#fff" }}>DUMMY</Text>
+        <Text style={{ color: "#fff" }}>DUMMY</Text>
+        <Text style={{ color: "#fff" }}>DUMMY</Text>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    justifyContent: "space-between",
+    alignItems: "center",
+    flexDirection: "row",
+    marginHorizontal: 20,
+  },
+  iconsContainer: {
+    flexDirection: "row",
+  },
   logo: {
-    width: 100,
-    height: 50,
+    width: 150,
+    height: 100,
     resizeMode: "contain",
+  },
+  icons: {
+    width: 30,
+    height: 30,
   },
 });
 export default Header;
